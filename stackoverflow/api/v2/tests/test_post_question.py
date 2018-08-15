191b9c9986222d44505a5a571b888530a9af9acc
asheuh <asheuh49@gmail.com>
2018-08-15 11:27:48 +0300

    #159757984 add tests for get all questions endpoint

--- stackoverflow/api/v2/tests/test_post_question.py
+++ stackoverflow/api/v2/tests/test_post_question.py
@@ -32,6 +32,44 @@ class TestUserQuestions(BaseTestCase):
             self.assertTrue(response_data['message'] == 'Question posted successfully')
             self.assertEqual(response.status_code, 201)
 
+    def test_user_retrieves_all_questions(self):
+        with self.client:
+            resp_register = self.client.post(
+                '/api/v2/auth/register',
+                data=json.dumps(dict(
+                    name='Brian Mboya',
+                    email='asheuh@gmail.com',
+                    username='asheuh',
+                    password='mermaid'
+                )),
+                content_type='application/json'
+            )
+            response = self.client.post(
+                '/api/v2/questions',
+                headers=dict(
+                    Authorization='Bearer ' + json.loads(
+                        resp_register.data.decode()
+                    )['Authorization']['access_token']
+                ),
+                data=json.dumps(dict(
+                    title='Gjango cli',
+                    description='How to create cli project in django?'
+                )),
+                content_type='application/json'
+            )
+            resp = self.client.get(
+                '/api/v2/questions',
+                headers=dict(
+                    Authorization='Bearer ' + json.loads(
+                        resp_register.data.decode()
+                    )['Authorization']['access_token']
+                )
+            )
+            response_data = json.loads(resp.data.decode())
+            print(response_data)
+            self.assertTrue(response_data['status'] == 'success')
+            self.assertEqual(resp.status_code, 200)
+
     def test_user_can_post_answer_to_a_question(self):
         with self.client:
             resp_register = self.client.post(
